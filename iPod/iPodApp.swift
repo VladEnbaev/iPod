e54d2b42274c97db4308772070d7588346a61fab
v.enbaev <v.enbaev@iva.ru>
2025-06-18 19:45:56 +0300

    Initial Commit

---
+++ iPod/iPodApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPodApp.swift
+//  iPod
+//
+//  Created by v.enbaev on 18.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct iPodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
